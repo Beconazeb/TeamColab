446c828fb9fbdc979c90fae9da2aaf5fa309c9c8
marianny <marianny2492@outlook.com>
2023-01-14 11:00:09 -0500

    Commit 2

--- src/Marianny/marianny.java
+++ src/Marianny/marianny.java
@@ -3,5 +3,6 @@ package Marianny;
 public class marianny {
     public static void main(String[] args) {
         System.out.println("GitHub is confusing");
+        System.out.println("We will get better with practice");
     }
 }
